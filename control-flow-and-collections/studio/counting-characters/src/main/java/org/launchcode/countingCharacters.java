980e11af9f6ce0e6f6b6d96a85ff9e2364204712
Riley <ririwilliams.rw@gmail.com>
2023-08-24 20:29:21 -0500

    Studio for Counting Characters

---
+++ control-flow-and-collections/studio/counting-characters/src/main/java/org/launchcode/countingCharacters.java
@@ -0,0 +1,31 @@
+package org.launchcode;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class countingCharacters {
+
+    public static void main(String[] args) {
+//        String quote = "The man in black fled across the desert, and the gunslinger followed";
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your quote: ");
+        String quote = input.nextLine();
+        char[] charactersInQuote = quote.toLowerCase().toCharArray();
+        HashMap<Character, Integer> countingCharacters = new HashMap<>();
+
+
+
+        for (char letter : charactersInQuote){
+            if () {
+                int count = countingCharacters.containsKey(letter) ? countingCharacters.get(letter) : 0;
+                countingCharacters.put(letter, count + 1);
+            }
+        }
+
+        for (Map.Entry<Character, Integer> character : countingCharacters.entrySet()) {
+            System.out.println(character.getKey() + ": " + character.getValue());
+        }
+    }
+}
+
